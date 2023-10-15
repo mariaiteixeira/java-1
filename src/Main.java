1854907d9207e09d9e20773054dd914094b0d876
Maria Isabel <mipt1612@gmail.com>
2023-10-15 01:17:57 -0300

    Adicionar sinopse de Top Gun

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,13 @@ public class Main {
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
         boolean incluidoNoPlano = true;
         double notaDoFilme = 8.1;
+
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+        Filme de aventura com galã dos anos 80
+        """;
+        System.out.println(sinopse);
     }
 }
